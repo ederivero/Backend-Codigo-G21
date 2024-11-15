03e8b89d544af0e1e1fd298b3f98bfe1965a226b
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2024-11-14 21:03:22 -0500

    inicios en python

---
+++ dia_1/1_variables.py
@@ -0,0 +1,29 @@
+# Tipos de datos de variables primitivas
+# String 
+nombre = "Farit o'Conner"
+direccion = 'Las Lechuzas "512'
+# Cuando queremos guardar un texto muy largo que ademas puede tener saltos de linea
+enunciado = '''Hola
+como estas
+        Te extraño
+Espero verte pronto,
+
+Saludos
+
+Eduardo'''
+
+otro_enunciado = """Hola
+como estas
+        Te extraño
+Espero verte pronto,
+
+Saludos
+
+Eduardo"""
+
+# Int
+edad = 32
+# Boolean
+casado = True
+# Float
+estatura=1.82
