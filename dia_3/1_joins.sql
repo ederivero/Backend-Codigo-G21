22c9cf18b959ffa8198cc0a7cb8b5f7ad620f0f5
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2024-11-22 23:05:48 -0500

    uso de funciones, trigger y transacciones

--- dia_3/1_joins.sql
+++ dia_3/1_joins.sql
@@ -98,3 +98,52 @@ ELSE 'HUBO UN ERROR'
 END,
 activo
 FROM clientes;
+
+SELECT CASE activo
+WHEN TRUE THEN 'ESTA ACTIVO EL CLIENTE'
+WHEN FALSE THEN 'EL CLIENTE NO PUEDE HACER OPERACIONES'
+ELSE 'HUBO UN ERROR'
+END,
+activo
+FROM clientes;
+
+-- Usando el switch case Mostrar los movimientos que sean DEPOSITO, TRANSFERENCIA o RETIRO, siendo:
+-- DEPOSITO: Cuando no hay cuenta_origen pero si cuenta destino
+-- TRANSFERENCIA: Cuando hay cuenta_origen y cuenta_destino
+-- RETIRO : Cuando hay cuenta_origen y no hay cuenta_destino
+-- y sus montos
+
+SELECT CASE
+WHEN cuenta_origen IS NULL AND cuenta_destino IS NOT NULL THEN 'DEPOSITO'
+WHEN cuenta_origen IS NOT NULL AND cuenta_destino IS NOT NULL THEN 'TRANSFERENCIA'
+WHEN cuenta_origen IS NOT NULL AND cuenta_destino IS NULL THEN 'RETIRO'
+END,
+monto
+FROM movimientos;
+
+
+-- En base al correo de los clientes hacer lo siguiente
+-- Si el correo es gmail > 'ES UNA PERSONA JOVEN'
+-- Si el correo es hotmail > 'ES UNA PERSONA ADULTA'
+-- Si el correo es yahoo > 'ES UN DINOSAURIO'
+-- PISTA: Usar el like en el CASE
+SELECT correo,
+    CASE
+        WHEN correo ILIKE '%gmail.com' THEN 'ES UNA PERSONA JOVEN'
+        WHEN correo ILIKE '%hotmail.com' THEN 'ES UNA PERSONA ADULTA'
+        WHEN correo ILIKE '%yahoo%' THEN 'ES UN DINOSAURIO'
+        ELSE 'DOMINIO DESCONOCIDO'
+    END AS calculo_edad_persona
+FROM clientes;
+
+-- Usando la funcion de agregacion SUM obtener los debitos de todas cuenta (lo que sale) cuenta_origen NO ES NULA
+SELECT cuenta_origen, SUM(monto)
+FROM movimientos
+WHERE cuenta_origen IS NOT NULL
+GROUP BY cuenta_origen;
+
+-- Obtener los creditos de todas las cuentas (lo que llega / entra) > cuanta_destino NO ES NULA
+SELECT cuenta_destino, SUM(monto)
+FROM movimientos
+WHERE cuenta_destino IS NOT NULL
+GROUP BY cuenta_destino;
