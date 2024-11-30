184ec7a1a6f6f3fe1a756aadc0ba976a72bcd584
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2024-11-29 21:18:13 -0500

    agregando migraciones a nuestro proyecto de flask

--- 2_flask_con_orm.py
+++ 2_flask_con_orm.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 # https://docs.sqlalchemy.org/en/20/core/type_basics.html#generic-camelcase-types
 from sqlalchemy import Column, types
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
@@ -9,6 +10,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg://postgres:root@localhost:5432/bd_flask'
 
 conexion = SQLAlchemy(app=app)
+
+# Luego de establecer la conexion utilizando SQLAlchemy ahora utilizamos la clase Migrate
+Migrate(app=app, db=conexion)
 
 
 # Cada tabla que vayamos a crear sera como una clase
@@ -25,17 +29,63 @@ class ProductoModel(conexion.Model):
     # serie ... not null unique > SQL
     serie = Column(type_=types.Text, nullable=False, unique=True)
     disponible = Column(type_=types.Boolean, nullable=True)
+    # name > si queremos manejar un nombre en el backend y otro nombre en la bd con la propiedad name indicamos como se llamara en la bd
+    fechaVencimiento = Column(
+        type_=types.Date, nullable=True, name='fecha_vencimiento')
+
+    # para indicar como queremos que se llame la tabla sin modificar el nombre de la clase
+    __tablename__ = 'productos'
 
 
-@app.route('/crear-tablas')
-def crear_tablas():
-    # creara todas las tablas que no esten en la base de datos
-    # si la tabla ya existe no la creara aun asi dentro de la tabla tenga modificaciones como agregar columna, quitar columna, relaciones, etc
-    # SIGUIENTE PASO: Usar migraciones
-    conexion.create_all()
-    return {
-        'message': 'Las tablas fueron creadas exitosamente'
-    }
+# @app.route('/crear-tablas')
+# def crear_tablas():
+#     # creara todas las tablas que no esten en la base de datos
+#     # si la tabla ya existe no la creara aun asi dentro de la tabla tenga modificaciones como agregar columna, quitar columna, relaciones, etc
+#     # SIGUIENTE PASO: Usar migraciones
+#     conexion.create_all()
+#     return {
+#         'message': 'Las tablas fueron creadas exitosamente'
+#     }
 
 
-app.run(debug=True)
+@app.route('/productos', methods=['POST', 'GET'])
+def gestion_productos():
+    metodo = request.method
+    if metodo == 'POST':
+        # primero leeremos la informacion proveniente del cliente
+        # convierte el json entrante a un diccionario para que pueda ser leido en python
+        data = request.get_json()
+
+        # Inicializamos nuestro nuevo registro
+        nuevoProducto = ProductoModel(nombre=data.get('nombre'),
+                                      precio=data.get('precio'),
+                                      serie=data.get('serie'),
+                                      disponible=data.get('disponible'),
+                                      fechaVencimiento=data.get('fechaVencimiento'))
+
+        print('Producto antes de guardarse en la bd', nuevoProducto.id)
+        # utilizamos la conexion para conectarnos a la bd
+        # empezamos una transaccion en la cual estamos indicando que agregaremos este registro
+        conexion.session.add(nuevoProducto)
+
+        # indicamos que los cambios tiene que guardarse de manera permanente
+        conexion.session.commit()
+
+        print('Producto luego de guardarse en la bd', nuevoProducto.id)
+        return {
+            'message': 'Producto creado exitosamente'
+        }
+
+    elif metodo == 'GET':
+        # Establecemos una consulta de obtencion de datos
+        # SELECT * FROM productos;
+        productos = conexion.session.query(ProductoModel).all()
+
+        print(productos)
+        return {
+            'message': 'Los productos son'
+        }
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
