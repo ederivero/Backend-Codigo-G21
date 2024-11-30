270de738d23a0fee696cef3b51cfe47ddb8398ec
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2024-11-29 22:59:16 -0500

    validadores con serializadores

--- 2_flask_con_orm.py
+++ 2_flask_con_orm.py
@@ -3,6 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 # https://docs.sqlalchemy.org/en/20/core/type_basics.html#generic-camelcase-types
 from sqlalchemy import Column, types
 from flask_migrate import Migrate
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow.exceptions import ValidationError
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 
@@ -37,6 +40,13 @@ class ProductoModel(conexion.Model):
     __tablename__ = 'productos'
 
 
+class ProductoSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        # Sirve para pasarle informacion a la clase de la cual estamos heredando pero sin la necesidad de modificar como tal la instancia de la clase
+        model = ProductoModel
+        # al indicar el modelo que se tiene que basar podra ubicar todos los atributos y ver sus restricciones (not null, unico, tipo de datos, etc)
+
+
 # @app.route('/crear-tablas')
 # def crear_tablas():
 #     # creara todas las tablas que no esten en la base de datos
@@ -52,49 +62,72 @@ class ProductoModel(conexion.Model):
 def gestion_productos():
     metodo = request.method
     if metodo == 'POST':
+        serializador = ProductoSchema()
         # primero leeremos la informacion proveniente del cliente
         # convierte el json entrante a un diccionario para que pueda ser leido en python
         data = request.get_json()
 
-        # Inicializamos nuestro nuevo registro
-        nuevoProducto = ProductoModel(nombre=data.get('nombre'),
-                                      precio=data.get('precio'),
-                                      serie=data.get('serie'),
-                                      disponible=data.get('disponible'),
-                                      fechaVencimiento=data.get('fechaVencimiento'))
+        # validara la informacion proveniente del frontend y nos dara un error si no cumple los requisitos
+        try:
+            serializador.load(data)
 
-        print('Producto antes de guardarse en la bd', nuevoProducto.id)
-        # utilizamos la conexion para conectarnos a la bd
-        # empezamos una transaccion en la cual estamos indicando que agregaremos este registro
-        conexion.session.add(nuevoProducto)
+            # Inicializamos nuestro nuevo registro
+            nuevoProducto = ProductoModel(nombre=data.get('nombre'),
+                                          precio=data.get('precio'),
+                                          serie=data.get('serie'),
+                                          disponible=data.get('disponible'),
+                                          fechaVencimiento=data.get('fechaVencimiento'))
 
-        # indicamos que los cambios tiene que guardarse de manera permanente
-        conexion.session.commit()
+            print('Producto antes de guardarse en la bd', nuevoProducto.id)
+            # utilizamos la conexion para conectarnos a la bd
+            # empezamos una transaccion en la cual estamos indicando que agregaremos este registro
+            conexion.session.add(nuevoProducto)
 
-        print('Producto luego de guardarse en la bd', nuevoProducto.id)
-        return {
-            'message': 'Producto creado exitosamente'
-        }
+            # indicamos que los cambios tiene que guardarse de manera permanente
+            conexion.session.commit()
+
+            print('Producto luego de guardarse en la bd', nuevoProducto.id)
+            return {
+                'message': 'Producto creado exitosamente'
+            }
+
+        except ValidationError as error:
+            # si falla al momento de hacer la validacion con el serializador
+            return {
+                'message': 'Error al crear el producto',
+                'content': error.args
+            }
+
+        except IntegrityError as error:
+            # si falla al momento de guardar en la base de datos y el producto ya existe (nombre es unico)
+            return {
+                'message': 'Error al crear el producto',
+                'content': 'El producto ya existe!'
+            }
 
     elif metodo == 'GET':
         # Establecemos una consulta de obtencion de datos
         # SELECT * FROM productos;
         productos = conexion.session.query(ProductoModel).all()
 
-        print(productos)
-        print(productos[0].id)
-        resultado = []
+        serializador = ProductoSchema()
+        # cuando convertimos la informacion desde la bd a un dict utilizamos el metodo dump
+        # cuando pasamos un arreglo de instancias tenemos que agregar el parametro many=True
+        # ese parametro ayuda a que itere la lista y convierta cada uno de las instancias
+        resultado = serializador.dump(productos, many=True)
+
+        # resultado = []
         # convertir la informacion de la lista de instancias a diccionario
-        for producto in productos:
-            producto_dict = {
-                'id': producto.id,
-                'nombre': producto.nombre,
-                'precio': producto.precio,
-                'serie': producto.serie,
-                'disponible': producto.disponible,
-                'fechaVencimiento': producto.fechaVencimiento
-            }
-            resultado.append(producto_dict)
+        # for producto in productos:
+        #     producto_dict = {
+        #         'id': producto.id,
+        #         'nombre': producto.nombre,
+        #         'precio': producto.precio,
+        #         'serie': producto.serie,
+        #         'disponible': producto.disponible,
+        #         'fechaVencimiento': producto.fechaVencimiento
+        #     }
+        #     resultado.append(producto_dict)
 
         return {
             'message': 'Los productos son',
