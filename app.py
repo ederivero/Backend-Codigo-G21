742772c71257de955447adca41fdece8411a7e89
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2024-12-11 21:18:43 -0500

    inicio semana 05

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from instancias import conexion
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL_PRINCIPAL')
+
+# Aca agregamos las otras conexiones a otras bases de datos secundarias
+app.config['SQLALCHEMY_BINDS'] = {
+    'mysql': environ.get('DATABASE_URL_MYSQL')
+}
+
+conexion.init_app(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
