f3aa48cff8efb87aa1fd5437d02ff62728493dbe
Eduardo Ramiro de Rivero Manrique <ederiveroman@gmail.com>
2025-01-23 21:56:09 -0500

    modificaciones APP

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-import express from "express";
-import pg from "pg";
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
+const express = require("express");
+const pg = require("pg");
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+// Definiendo los cors
+app.use(
+  cors({
+    methods: ["GET", "POST"], // '*'
+    origin: ["http://mifrontend.com", "http://localhost:8080"], // '*'
+    allowedHeaders: ["Authorization", "Content-Type", "Accept"], // '*'
+  })
+);
 
+app.route("/").get((req, res) => {
+  return res.json({
+    content: process.env,
+    message: "Bienvenido a mi API",
+  });
+});
 const conexion = new pg.Pool({
+  // connectionString: `pg://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`,
   host: process.env.RDS_HOSTNAME,
   port: process.env.RDS_PORT,
   database: process.env.RDS_DB_NAME,
   user: process.env.RDS_USERNAME,
   password: process.env.RDS_PASSWORD,
+  ssl: true,
 });
 
 app
