1238b9aacc2b5eb107338211c169d5109992f33d
Eduardo de Rivero <eduardomanrique@ravn.co>
2025-01-24 19:21:13 -0500

    modificacion de comillas

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
-const express = require("express");
-const pg = require("pg");
-const cors = require("cors");
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
+const express = require('express')
+const pg = require('pg')
+const cors = require('cors')
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 // Definiendo los cors
 app.use(
   cors({
-    methods: ["GET", "POST"], // '*'
-    origin: "*", //["http://mifrontend.com", "http://localhost:8080"], // '*'
-    allowedHeaders: ["Authorization", "Content-Type", "Accept"], // '*'
+    methods: ['GET', 'POST'], // '*'
+    origin: '*', //["http://mifrontend.com", "http://localhost:8080"], // '*'
+    allowedHeaders: ['Authorization', 'Content-Type', 'Accept'], // '*'
   })
-);
+)
 
-app.route("/").get((req, res) => {
+app.route('/').get((req, res) => {
   return res.json({
     content: process.env,
-    message: "Bienvenido a mi API",
-  });
-});
+    message: 'Bienvenido a mi API',
+  })
+})
+
 const conexion = new pg.Pool({
   // connectionString: `pg://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`,
   host: process.env.RDS_HOSTNAME,
@@ -28,41 +29,39 @@ const conexion = new pg.Pool({
   user: process.env.RDS_USERNAME,
   password: process.env.RDS_PASSWORD,
   ssl: true,
-});
+})
 
 app
-  .route("/productos")
+  .route('/productos')
   .get(async (req, res) => {
-    const result = await conexion.query("SELECT * FROM productos");
-    const registros = result.rows;
+    const result = await conexion.query('SELECT * FROM productos')
+    const registros = result.rows
 
     return res.json({
       content: registros,
-    });
+    })
   })
   .post(async (req, res) => {
-    const data = req.body;
-    const nuevoProducto = await conexion.query(
-      "INSERT INTO productos (nombre, precio) VALUES ($1,$2) RETURNING *",
-      [data.nombre, data.precio]
-    );
+    const data = req.body
+    const nuevoProducto = await conexion.query('INSERT INTO productos (nombre, precio) VALUES ($1,$2) RETURNING *', [
+      data.nombre,
+      data.precio,
+    ])
 
-    const resultado = nuevoProducto.rows;
+    const resultado = nuevoProducto.rows
 
     return res.json({
-      message: "Producto creado exitosamente",
+      message: 'Producto creado exitosamente',
       content: resultado,
-    });
-  });
+    })
+  })
 
 app.listen(process.env.PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
+  console.log(`Servidor corriendo en el puerto ${process.env.PORT}`)
 
   Promise.all([
-    conexion.query(
-      "CREATE TABLE IF NOT EXISTS productos (id SERIAL PRIMARY KEY, nombre TEXT, precio FLOAT)"
-    ),
+    conexion.query('CREATE TABLE IF NOT EXISTS productos (id SERIAL PRIMARY KEY, nombre TEXT, precio FLOAT)'),
   ]).then(() => {
-    console.log("Las tablas fueron creadas exitosamente");
-  });
-});
+    console.log('Las tablas fueron creadas exitosamente')
+  })
+})
